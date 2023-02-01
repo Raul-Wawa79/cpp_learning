2f3f9c9dd38711254afe3256700a3b3c19caa8ab
Raul-Wawa79 <89007574+Raul-Wawa79@users.noreply.github.com>
2023-02-01 16:20:58 +0100

    Add ASCII for Paula

---
+++ paula.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+std::cout << "\n";
+std::cout << "\n";
+std::cout << "\n";
+std::cout << " PPPPP    A   U   U L       A  \n";
+std::cout << " P   P   AAA  U   U L      AAA \n";
+std::cout << " P   P  A   A U   U L     A   A\n";
+std::cout << " PPPPP  AAAAA U   U L     AAAAA\n";
+std::cout << " P      A   A U   U L     A   A\n";
+std::cout << " P      A   A U   U L     A   A\n";
+std::cout << " P      A   A UUUUU LLLLL A   A\n";
+std::cout << "\n";
+std::cout << "\n";
+std::cout << "\n";
+}
